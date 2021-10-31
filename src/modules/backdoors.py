77bd7ec69c6aa237047d32b1107eeac35b0cfe30
Billal Fauzan <71764176+billalxcode@users.noreply.github.com>
2021-10-31 19:57:09 +0700

    Fix: no implement error

--- src/modules/backdoors.py
+++ src/modules/backdoors.py
@@ -1,15 +1,21 @@
 import os
 import sys
 
-moduleDir = os.path.join(os.getcwd(), "/src")
+moduleDir = os.path.join(os.getcwd())
 sys.path.insert(0, moduleDir)
 
 class Backdoors:
     def __init__(self) -> None:
-        pass
+        from src.terminal import Terminal
+
+        self.terminal = Terminal()
 
     def start(self):
-        pass
+        while True:
+            self.terminal.clear()
+            self.terminal.showLogo()
+            if self.terminal.showNoImplement():
+                break
     
 def run():
     apps = Backdoors()
